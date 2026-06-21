f5b060637ca2b5a4287f708d393cc7cca59f1061
STUpid <sharmashivansh.09812@gmail.com>
2026-06-21 20:45:44 +0530

    removed comments

--- slowapi.py
+++ slowapi.py
@@ -2,4 +2,4 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 
-SlowAPI = FastAPI # no one will know this is a joke, this is just a wrapper around fastapi to make it look like a slow api, but in reality it's just a normal fastapi app, the name is just for fun
+SlowAPI = FastAPI
